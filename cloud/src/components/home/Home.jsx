30b73df659aa397ffa7ea63b05be9b0ec70daa59
Rakshit Dabral <rakshitdabral1@gmail.com>
2023-04-23 10:31:43 +0530

    update

--- cloud/src/components/home/Home.jsx
+++ cloud/src/components/home/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Navbar from '../navbar/Navbar'
 import {Link} from "react-router-dom";
 import Footer from "../footer/Footer"
-import image_home from "./images/Top (2).png";
+import image_home from "./images/Top3.png";
 import image_ellipse from "./images/Ellipse.svg";
 import image_pdf from "./images/PDF.png";
 import image_jpg from "./images/JPG.png";
