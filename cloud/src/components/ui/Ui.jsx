c23c8c046d07f2f1d18205a7f13e5e15acbf9705
Rakshit Dabral <rakshitdabral1@gmail.com>
2023-05-23 09:34:33 +0530

    dashboard ui

--- cloud/src/components/ui/Ui.jsx
+++ cloud/src/components/ui/Ui.jsx
@@ -2,17 +2,37 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import "./ui.css";
+import ReactDOM from 'react-dom'
+  import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+  import { faSearch } from '@fortawesome/free-solid-svg-icons'
 
 function Ui() {
   return (
     <>
-      <section class="layout">
-        <div class="header">1</div>
-        <div class="leftSide">2</div>
-        <div class="body">3</div>
-        <div class="rightSide">4</div>
-        
-      </section>
+      <div class="container-fluid dashboard ">
+  <div class="row">
+    <div class="col">
+      CLOUD'O
+    </div>
+    <div class="col-lg-8 col-sm-5">
+      2 of 3 (wider)
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+  <div class="row">
+    <div class="col">
+      1 of 3
+    </div>
+    <div class="col-lg-8 col-sm-5">
+      2 of 3 (wider)
+    </div>
+    <div class="col">
+      3 of 3
+    </div>
+  </div>
+</div>
     </>
   );
 }
